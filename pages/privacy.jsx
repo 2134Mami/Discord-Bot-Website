c97731d6d7eb22851be110c0c277d11ce758664c
2134Mami <168979015+2134Mami@users.noreply.github.com>
2024-05-07 12:39:35 +0300

    Update privacy.jsx

--- pages/privacy.jsx
+++ pages/privacy.jsx
@@ -5,17 +5,16 @@ export default function Privacy() {
             <h1 className="font-semibold text-3xl text-white/75">Privacy Policy for SnowBOT</h1>
             <p className="text-sm text-white/30 mb-4">Last Update: 20/05/2022  🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
-        <p>⊳ To use the SnowBOT bot, you must define the necessary permissions when adding it to your discord server. <a className="text-white">(READ MESSAGES, SEND MESSAGES, EMBED LINKS, ATTACH FILES, READ MESSAGE HISTORY, CONNECT, SPEAK, USE VOICE ACTIVITY, PRIORITY SPEAKER)</a></p>
+        <p>⊳  SnowBOT botunu kullanmak için discord sunucunuza eklerken gerekli izinleri tanımlamanız gerekmektedir. <a className="text-white"></a></p>
         <div className="py-3"></div>
-        
         <div className="py-3"></div>
-        <p>⊳ Every user can use the SnowBOT bot however they want, as long as they do not intend to harm.</p>
+        <p>⊳ Her kullanıcı, zarar verme amacı taşımadığı sürece SnowBOT botunu dilediği gibi kullanabilir.</p>
         <div className="py-3"></div>
-        <p>⊳ You cannot use certain commands at the same time while using SnowBOT.</p>   
+        <p>⊳ nowBOT kullanırken belirli komutları aynı anda kullanamazsınız.</p>   
         <div className="py-3"></div>
-        <p>⊳ SnowBOT bot does not save any data based on users.</p>
+        <p>⊳ SnowBOT bot kullanıcıya dayalı hiçbir veriyi kaydetmez.</p>
         <div className="py-3"></div>
-        <p>⊳ While using the bot, your user ID is saved and deleted in the system for 60 seconds for once.</p> 
+        <p>⊳ Botu kullanırken kullanıcı kimliğiniz bir defaya mahsus olmak üzere 60 saniye süreyle sisteme kaydedilip silinir.</p> 
     
         </div>
             </div>
