cc49aa50ac46bbc64d50615242b4946d2fc6d319
2134Mami <168979015+2134Mami@users.noreply.github.com>
2024-05-07 13:32:14 +0300

    Update commands.jsx

--- pages/commands.jsx
+++ pages/commands.jsx
@@ -3,32 +3,15 @@ import Link from "next/link";
 
 export default function Commands() {
 const commands = [
-  "/autoplay",
-  "/back",
-  "/statistic",
-  "/channel",
-  "/clear",
-  "/dj",
-  "/filter",
-  "/help",
-  "/language",
-  "/loop",
-  "/nowplaying",
-  "/pause",
-  "/ping",
-  "/play",
-  "/playlist",
-  "/queue",
-  "/resume",
-  "/save",
-  "/search",
-  "/seek",
-  "/servers",
-  "/shuffle",
-  "/skip",
-  "/stop",
-  "/time",
-  "/volume"
+  "s!yardım",
+  "s!genel",
+  "s!ayarlar",
+  "s!öneri-sistem",
+  "s!seviye",
+  "s!kullanıcı",
+  "s!otorol-sistem",
+  "s!eklenti",
+  "s!eğlence",
 ];
 
   return (
